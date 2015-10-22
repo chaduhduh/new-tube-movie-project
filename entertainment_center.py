dc6ecb5753ce6cac0c03442c73476e3b27e82db3
chadd <chadd.myers3@gmail.com>
2015-10-21 22:30:34 -0700

    adding views directory

--- entertainment_center.py
+++ entertainment_center.py
@@ -22,16 +22,29 @@ oceans_eleven = media.Media({
 	"poster_image_url" : "./img/poster-image-oceans-eleven.jpg", 
 	"rating" : 5
 	})
-#add to list .. repeat below for the rest of the movies
+#add to list .. 
 addMovie(video_list, oceans_eleven)
+
+#repeat below for the rest of the movies
 sharknado = media.Media({
 	"title" : "Sharknado", 
 	"trailer_youtube_url" : "https://www.youtube.com/watch?v=1hEZkkqjSfs",
-	"storyline" : "storyline here", 
+	"storyline" : "Tornados filled with sharks... Enough Said!", 
 	"poster_image_url" : "./img/poster-image-sharknado.jpg", 
 	"rating" : 5
 	})
 addMovie(video_list, sharknado)
+dumb_and_dumber = media.Media({
+	"title" : "Dumb and Dumber", 
+	"trailer_youtube_url" : "https://www.youtube.com/watch?v=l13yPhimE3o",
+	"storyline" : "Two friends travel across the country to deliver a lost suit\
+	    case to a women. Jim Carrey and Jeff Daniels star in this comedy.",
+	"poster_image_url" : "./img/poster-image-dumb-and-dumber.jpg", 
+	"rating" : 5
+	})
+addMovie(video_list, dumb_and_dumber)
+
+
 
 #fill template with our data
 fresh_tomatoes.open_movies_page(video_list)
