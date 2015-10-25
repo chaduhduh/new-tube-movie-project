a19426620a54eb27bda391844aa74f04c1964b33
chaduhduh <chadd.myers3@gmail.com>
2015-10-24 21:16:20 -0700

    adding more movie data

--- entertainment_center.py
+++ entertainment_center.py
@@ -76,6 +76,62 @@ catch_me_if_you_can = media.Media({
 	})
 addMovie(video_list, catch_me_if_you_can)
 
+batman_dark_knight = media.Media({
+	"title" : "Batman the Dark Knight", 
+	"trailer_youtube_url" : "https://www.youtube.com/watch?v=EXeTwQWrcwY",
+	"storyline" : "With the help of allies Batman has been able to keep a tight \
+		lid on crime in Gotham City. But when the Joker suddenly throws the town \
+		into chaos, the caped Crusader begins to tread a fine line between \
+		heroism and vigilantism.",
+	"poster_image_url" : "./img/poster-image-batman-dark-knight.jpg", 
+    "rating" : 4
+	})
+addMovie(video_list, batman_dark_knight)
+
+atari_game_over = media.Media({
+	"title" : "Atari: Game Over", 
+	"trailer_youtube_url" : "https://www.youtube.com/watch?v=rIaWAyHIqok",
+	"storyline" : "When Atari's game ET massively fails so does the company.\
+		Legend states thousands of copies of the ET game were buried underground \
+		in a landfill, is ET to blame for the companies downfall?",
+	"poster_image_url" : "./img/poster-image-atari-game-over.jpg", 
+    "rating" : 2
+	})
+addMovie(video_list, atari_game_over)
+
+the_town = media.Media({
+	"title" : "The Town", 
+	"trailer_youtube_url" : "https://www.youtube.com/watch?v=WcXt9aUMbBk",
+	"storyline" : "Doug MacRay (Ben Affleck) leads a band of ruthless bank \
+		robbers, when he meets a girl he wants to leave the group, this doesnt \
+		go over well with the boss.",
+	"poster_image_url" : "./img/poster-image-the-town.jpg", 
+    "rating" : 4
+	})
+addMovie(video_list, the_town)
+
+star_wars_four = media.Media({
+	"title" : "Star Wars: A New Hope", 
+	"trailer_youtube_url" : "https://www.youtube.com/watch?v=1g3_CFmnU7k",
+	"storyline" : "The Imperial Forces -- under orders from cruel Darth \
+		Vader -- hold Princess Leia hostage, \
+		in their efforts to quell the rebellion against the Galactic Empire.",
+	"poster_image_url" : "./img/poster-image-star-wars-4.jpg", 
+    "rating" : 5
+	})
+addMovie(video_list, star_wars_four)
+
+ready_player_one = media.Media({
+	"title" : "Ready Player One : The Movie", 
+	"trailer_youtube_url" : "https://www.youtube.com/watch?v=4ni-9ydZPoA",
+	"storyline" : "Welcome to the OASIS, a hyper-realistic, 3D, videogame\
+	    paradise. It's 2045, and pretty much everyone logs in to the OASIS \
+	 	daily to escape their terrible lives, lives affected by overpopulation,\
+	  	unemployment, and energy shortages.",
+	"poster_image_url" : "./img/poster-image-ready-player-one.jpg", 
+    "rating" : 5
+	})
+addMovie(video_list, ready_player_one)
 
 
 #pass our movie list content to be used in page generation
